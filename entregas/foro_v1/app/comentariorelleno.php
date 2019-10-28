057f066080297372641f318b395df0e56a391efc
Jaime Zaloña <jaimezalo@gmail.com>
2019-10-28 12:21:27 +0100

    28/10/2019

--- entregas/foro_v1/app/comentariorelleno.php
+++ entregas/foro_v1/app/comentariorelleno.php
@@ -7,11 +7,9 @@
 		<div class="formulario">
 		<form action="index.php" method="post">
 			Tema:<br/>
-			<input type="text" name="tema" value="<?=(isset($_REQUEST['tema']))?$_REQUEST['tema']:''?>"><br>
+			<input type="text" name="tema" value="<?=(isset($_REQUEST['tema']))?strip_tags($_REQUEST['tema']):''?>"><br>
 			Comentario:<br/>
-			<textarea autofocus rows="8" cols="30" maxlength="300" name="comentario">
-				<?=(isset($_REQUEST['comentario']))?$_REQUEST['comentario']:''?>
-			</textarea><br/><br/>
+			<textarea autofocus rows="8" cols="60" maxlength="300" name="comentario" ><?=(isset($_REQUEST['comentario']))?strip_tags($_REQUEST['comentario']):''?></textarea><br/><br/>
 			<input type="submit" name="orden" value="Detalles">
 			<input type="submit" name="orden" value="Nueva opinión">
 			<input type="submit" name="orden" value="Terminar">
